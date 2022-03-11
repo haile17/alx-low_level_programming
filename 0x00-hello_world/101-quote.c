8e1a47aec7bcea9edaf900b6ff226838fbcb5022
haile17 <hailefikere17@gmail.com>
2022-03-11 00:15:05 -0800

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,12 @@
 #include <unistd.h>
 
 /**
- *  * main - prints a sentence followed by a new line
- *   *
- *    * Return: returns 1 after printing
- *    */
-
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-       	return (1);
+	return (1);
 }
