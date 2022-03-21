b3918b86137b0b012eef5bff8679135541a78b80
haile17 <hailefikere17@gmail.com>
2022-03-21 04:01:16 -0700

    pointers arrays strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,16 @@
 #include "main.h"
 
 /**
- * puts2 - print  every other character of a string
- * @str:pointer char
- * return:void
+ * puts2 - prints one char out of 2 of a string.
+ * @str: character value
  */
 void puts2(char *str)
 {
-	int i, x;
-	i = 0;
-	x = 0;
-	while (str[i++])
-	{
-		x++;
-	}
-	for (i = 0; i < x; i += 2)
-	{
-		_putchar(*(str + i));
-	}
+	int i, a;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	for (a = 0; a < i; a += 2)
+		_putchar(str[a]);
 	_putchar('\n');
 }
