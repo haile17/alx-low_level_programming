7ee4b31f71bcbd1edeec4614634500b66e1c3e63
haile17 <hailefikere17@gmail.com>
2022-03-21 04:04:19 -0700

    pointers arrays strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,36 +1,25 @@
 #include "main.h"
 
 /**
- * reverse_array - reverses an array of integers
- * @a: array to be reversed
- * @n: number of elements in the array
+ * print_rev - prints string
+ * @s: input string
+ *Return: no return
  */
+
 void print_rev(char *s)
 {
-	int i = 0;
-	int lenStr, c = 0;
-	char newStr;
-	char temp;
+	int count = 0;
 
-	while (s[i] != '\0')
+	while (*s != '\0')
 	{
-		i++;
+		s++;
+		count++;
 	}
-	lenStr = i;
-
-
-	for (i = 0; i < lenStr / 2; i++)
+	while (count > 0)
 	{
-		temp = s[i];
-		s[i] = s[lenStr - i - 1];
-		s[lenStr - i - 1] = temp;
+		s--;
+		_putchar(*s);
+		count--;
 	}
-
-	while (s[c] != '\0')
-	{
-		_putchar(s);
-		c++;
-	}
-
 	_putchar('\n');
 }
