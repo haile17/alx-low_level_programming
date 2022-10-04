0a2bc1e0ce94d181de0936f444fd07addadb2256
haile17 <hailefikere17@gmail.com>
2022-10-04 01:03:23 -0700

    exponential

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,6 +10,7 @@ int binary_search(int *array, size_t size, int value);
 int help_binary(int *array, int value, size_t low, size_t high);
 void array_print(int *array, size_t low, size_t high);
 int interpolation_search(int *array, size_t size, int value);
+int recursive_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
